97160e2b0c0294743aa586787ced9da119d64702
Charlie0560 <chaitanyalokhande87@gmail.com>
2021-10-23 17:59:14 +0530

    overs correction

--- index.js
+++ index.js
@@ -40,18 +40,35 @@ function rdecrement() {
   console.log(run);
 }
 function oincrement() {
-    over = over + 1;
-    point = over % 6;
-    main = over /6;
-    document.getElementById("main").innerHTML = parseInt(main) ;
-    document.getElementById("pointer").innerHTML= parseInt(point);
-    console.log(over);
+  over = over + 1;
+  point = over % 6;
+  main = over / 6;
+  document.getElementById("main").innerHTML = parseInt(main);
+  document.getElementById("pointer").innerHTML = parseInt(point);
+  console.log(over);
 }
 function odecrement() {
-    point = document.getElementById("pointer").innerHTML;
-    point = point - 1;
+  point = document.getElementById("pointer").innerHTML;
+  main = document.getElementById("main").innerHTML;
+  if (point >= 0) {
+    if ((point == 0) & (main > 0)) {
+      main--;
+      point = 5;
+      over--;
+      document.getElementById("pointer").innerHTML = point;
+      document.getElementById("main").innerHTML = main;
+    } else {
+      point--;
+      over--;
+      document.getElementById("pointer").innerHTML = point;
+      // document.getElementById("main").innerHTML = main;
+    }
+  } else {
+    point = 0;
+    over--;
     document.getElementById("pointer").innerHTML = point;
-    console.log(point);
+  }
+  console.log(main,point);
 }
 
 function resetfunction() {
